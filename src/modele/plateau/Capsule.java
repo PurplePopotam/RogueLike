73964dcac20aa7c0dbc2622375bcf4927c0ffa84
MODRZYK THIBAUT p1719782 <thibaut.modrzyk@etu.univ-lyon1.fr>
2021-03-31 19:10:10 +0200

    J'ai rajouté l'inventaire et l'orientation

---
+++ src/modele/plateau/Capsule.java
@@ -0,0 +1,11 @@
+package modele.plateau;
+
+public class Capsule extends EntiteStatique{
+
+    public Capsule(Jeu _jeu){ super(_jeu);}
+
+    @Override
+    public boolean traversable() {
+        return true;
+    }
+}

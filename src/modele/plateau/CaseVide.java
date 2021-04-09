fcc81491ac8e0d51cc4f70852550e5b65b0d1ede
Thibaut Modrzyk <52449813+PurplePopotam@users.noreply.github.com>
2021-04-09 14:46:03 +0200

    Correction

--- src/modele/plateau/CaseVide.java
+++ src/modele/plateau/CaseVide.java
@@ -3,10 +3,8 @@ package modele.plateau;
 public class CaseVide extends EntiteStatique {
     public CaseVide(Jeu _jeu) { super(_jeu); }
 
-        @Override
-        public boolean traversable() {
-            return false;
-        }
-
+    @Override
+    public boolean traversable() {
+        return false;
     }
 }
